f4648befe8cf369d75d9aa4f0e10c1964e99122e
Izaias Carmo Ignacio <izaias.ignacio@cd.com>
2019-11-28 17:33:34 -0300

    home

--- resources/views/colecao.blade.php
+++ resources/views/colecao.blade.php
@@ -6,6 +6,7 @@
         <title>ICIGames</title>
         <link rel="stylesheet" href="{{URL::asset('public/css/bulma.min.css')}}" />
         <link rel="stylesheet" href="{{URL::asset('public/css/games.css')}}" />
+        <link rel="stylesheet" href="{{URL::asset('public/css/font-awesome.css')}}">
     </head>
     <body>
         <nav class="navbar is-topo" role="navigation" aria-label="main navigation">
@@ -15,30 +16,103 @@
             <div class="container is-fluid container_topo">
                 <div id="" class="navbar-menu">
                     <div class="navbar-start">
-                        <a class="navbar-item">Dashboard</a>
+                        <a class="navbar-item">Dashboard<i class="fas fa-accusoft"></i></a>
                         <a class="navbar-item">Coleção</a>
                         <a class="navbar-item">Wishlist</a>
                         <a class="navbar-item">Watchlist</a>
+                        <a class="navbar-item">Game Pass</a>
                         <a class="navbar-item">Plus</a>
                     </div>
                 </div>
             </div>
-            <!-- <div class="navbar-end">
+            <div class="navbar-end">
                 <div class="navbar-item">
                     <div class="buttons">
-                        <a class="button is-primary">
+                        <a class="button is-info">
+                            <span class="icon is-small">
+                                <i class="fas fa-accusoft"></i>
+                            </span>
                             <strong>Adicionar</strong>
                         </a>
                     </div>
                 </div>
-            </div> -->
+            </div>
         </nav>
-        <aside class="menu">
-            <p class="menu-label">General</p>
-            <ul class="menu-list">
-                <li><a>Dashboard</a></li>
-                <li><a>Customers</a></li>
-            </ul>
-        </aside>
+        <div class="columns">
+            <div class="column coluna_menu">
+                <aside class="menu">
+                    <p class="menu-label">Plataformas</p>
+                    <ul class="menu-list">
+                        <li><a>Todas</a></li>
+                        <li><a>Xxxxxxxxxxx X</a></li>
+                    </ul>
+                </aside>
+            </div>
+            <div class="column is-11">
+                <br>
+                <div class="columns">
+                    <div class="column is-2 coluna_thumb">
+                        <div class="card">
+                            <div class="card-image">
+                                <figure class="image is-2by3">
+                                    <img src="https://versions.bulma.io/0.7.1/images/placeholders/1280x960.png">
+                                </figure>
+                                <div class="stripe">Red trousers</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="column is-2 coluna_thumb">
+                        <div class="card">
+                            <div class="card-image">
+                                <figure class="image is-2by3">
+                                    <img src="https://versions.bulma.io/0.7.1/images/placeholders/1280x960.png">
+                                </figure>
+                                <div class="stripe">Red trousers</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="column is-2 coluna_thumb">
+                        <div class="card">
+                            <div class="card-image">
+                                <figure class="image is-2by3">
+                                    <img src="https://versions.bulma.io/0.7.1/images/placeholders/1280x960.png">
+                                </figure>
+                                <div class="stripe">Red trousers</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="column is-2 coluna_thumb">
+                        <div class="card">
+                            <div class="card-image">
+                                <figure class="image is-2by3">
+                                    <img src="https://versions.bulma.io/0.7.1/images/placeholders/1280x960.png">
+                                </figure>
+                                <div class="stripe">Red trousers</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="column is-2 coluna_thumb">
+                        <div class="card">
+                            <div class="card-image">
+                                <figure class="image is-2by3">
+                                    <img src="https://versions.bulma.io/0.7.1/images/placeholders/1280x960.png">
+                                </figure>
+                                <div class="stripe">Red trousers</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="column is-2 coluna_thumb">
+                        <div class="card">
+                            <div class="card-image">
+                                <figure class="image is-2by3">
+                                    <img src="https://versions.bulma.io/0.7.1/images/placeholders/1280x960.png">
+                                </figure>
+                                <div class="stripe">Red trousers</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </body>
 </html>
