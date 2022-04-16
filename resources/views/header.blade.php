5f12d3b221d96114f7482d90e878a9b1a7764c64
Izaias <izaias.ignacio@outlook.com>
2022-04-16 17:29:07 -0300

    favicon

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,6 +1,7 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title>ICIGames</title>
+<link rel="shortcut icon" href="{{URL::asset('public/favicon.ico')}}">
 <link rel="stylesheet" href="{{URL::asset('public/css/bulma.min.css')}}" />
 <link rel="stylesheet" href="{{URL::asset('public/css/bulma-badge.min.css')}}" />
 <link rel="stylesheet" href="{{URL::asset('public/css/font-awesome.css')}}" />
